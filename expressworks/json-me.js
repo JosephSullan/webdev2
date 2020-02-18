64341719bc6af0d7935521501e98be5c0f90b392
JosephSullan <58454989+JosephSullan@users.noreply.github.com>
2020-02-18 20:52:17 +0800

    fourth-commit

--- expressworks/json-me.js
+++ expressworks/json-me.js
@@ -1,15 +1,3 @@
-/*
-    Most of the times we're building RESTful API servers with JSON.
-
-    Write a server that, when it receives a GET, reads a file, parses it to JSON,
-    and responds with that content to the user.
-
-    The server should respond to any GET that matches the /books resource path.
-    As always, the port is passed in process.argv[2]. The file to read is passed
-    in process.argv[3].
-*/
-
-
 const express = require('express');
 const app = express();
 const fs = require('fs');
--- expressworks/static.js
+++ expressworks/static.js
@@ -1,11 +1,3 @@
-/*
-    This exercise is about serving static assets like HTML files.
-    There are many ways to do it, but we want you to apply static middleware to serve the file index.html.
-
-    Please don't use ANY routes like app.get. ONLY static.
-*/
-
-
 const express = require('express');
 const app = express();
 const path = require('path');
